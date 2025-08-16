9db1445b1520b2de5bfef69c01638ba1837431d6
Josue Flores <josue.flores@zebrasoft.io>
2025-08-16 10:16:38 -0600

    feat; new tables

---
+++ StoreX.Domain/Entities/Movement.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace StoreX.Domain.Entities
+{
+    public class Movement
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public long MovementId { get; set; }
+        //public byte MovementType
+        [Required]
+        public DateTime CreatedAt { get; set; }
+
+        [Required]
+        public int CreatedBy { get; set; }
+
+        [JsonIgnore]
+        [ForeignKey("CreatedBy")]
+        public User? User { get; set; }
+    }
+}
