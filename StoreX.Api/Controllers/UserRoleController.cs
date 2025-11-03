6b8827b6becdd5d385ba5e9bce34b6dc07203e03
Josue Flores <josue.omar.75@hotmail.com>
2025-11-02 23:13:06 -0600

    fix

--- StoreX.Api/Controllers/UserRoleController.cs
+++ StoreX.Api/Controllers/UserRoleController.cs
@@ -29,4 +29,40 @@ namespace StoreX.Api.Controllers
         [HttpGet("{id:int}", Name = "GetUserRoleById")]
         [ProducesResponseType(typeof(UserRole), StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
-        public async Task<IActionResult> GetUserR
+        public async Task<IActionResult> GetUserRoleById(int id, CancellationToken cancellationToken)
+        {
+            var item = await _userRoleService.GetByIdAsync(id, cancellationToken);
+            if (item == null) return NotFound($"No se encontró un user role con ID {id}");
+            return Ok(item);
+        }
+
+        [HttpPost(Name = "CreateUserRole")]
+        [ProducesResponseType(typeof(UserRole), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<IActionResult> CreateUserRole(UserRole userRole, CancellationToken cancellationToken)
+        {
+            var created = await _userRoleService.AddAsync(userRole, cancellationToken);
+            return Ok(created);
+        }
+
+        [HttpPut("{id:int}", Name = "UpdateUserRole")]
+        [ProducesResponseType(typeof(UserRole), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> UpdateUserRole(int id, UserRole userRole, CancellationToken cancellationToken)
+        {
+            var updated = await _userRoleService.UpdateAsync(userRole, cancellationToken);
+            if (updated == null) return NotFound($"No se encontró un user role con ID {id}");
+            return Ok(updated);
+        }
+
+        [HttpDelete("{id:int}", Name = "DeleteUserRole")]
+        [ProducesResponseType(typeof(bool), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> DeleteUserRole(int id, CancellationToken cancellationToken)
+        {
+            var deleted = await _userRoleService.DeleteAsync(id, cancellationToken);
+            if (!deleted) return NotFound($"No se encontró un user role con ID {id}");
+            return Ok(true);
+        }
+    }
+}
